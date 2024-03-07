6cdd1209e96073444e4a708ff9b47bea76cdb602
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-07 09:58:11 -0300

    recuperando dados API github

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,36 +3,60 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import {  faArrowUpRightFromSquare, faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'; // Observe o uso do free-solid-svg-icons
 import { Header, ProfileContainer, ProfileInfos, SocialMedia } from './styles';
+import { api } from '../../lib/axios';
+import { useEffect, useState } from 'react';
+
+interface ProfileProps {
+  avatar_url: string
+  name: string
+  html_url: string
+  bio: string
+  login: string
+  company: string
+  followers: number
+}
 export function Profile() {
+
+  const [userData, setUserData] = useState<ProfileProps>();
+
+  async function fetchUser() {
+    const response = await api.get('users/MilenaRizzi');
+    const data = response.data;
+
+    setUserData(data);
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, [])
+
   return (
     <ProfileContainer>
-      <img src="src\assets\foto-perfil.png" alt="" />
+      <img src={userData?.avatar_url} alt="" />
       <ProfileInfos>
         <Header>
-          <h1>Cameron Williamson</h1>
+          <h1>{userData?.name}</h1>
           <span>
-            <a href="">Github</a>
+            <a href={userData?.html_url}  target="_blank">Github</a>
            <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" style={{color: "#3294f8",}} />
           </span>
 
         </Header>
         <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
+          {userData?.bio}
         </p>
         <SocialMedia>
           <div>
             <FontAwesomeIcon icon={faGithub} style={{color: "#3a536b",}}/>
-            <span>cameronsill</span>
+            <span>{userData?.login}</span>
           </div>
           <div>
           <FontAwesomeIcon icon={faBuilding} style={{color: "#3a536b",}}/>
-            <span>Rocketseat</span>
+            <span>{userData?.company}</span>
           </div>
           <div>
           <FontAwesomeIcon icon={faUserGroup} style={{color: "#3a536b",}}/>
-            <span>32 seguidores</span>
+          <span>{userData?.followers === 0 ? 0 : userData?.followers} seguidores</span>
           </div>
         </SocialMedia>
       </ProfileInfos>
