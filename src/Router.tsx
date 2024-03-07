d1fb3d9b9f66e11519de65af14b8ce7732ec77f3
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-07 19:17:58 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,11 +4,12 @@ import { Post } from "./pages/Post";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
+
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout/>}>
         <Route path="/" element={<Blog/>}/>
-        <Route path="/post" element={<Post/>}/>
+        <Route path="/post/:issueNumber" element={<Post/>}/>
       </Route>
     </Routes>
   )
