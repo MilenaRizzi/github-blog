079baef32daa2fd4d7157315306842ef68f42b35
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-07 07:41:43 -0300

    criei campo pesquisa

---
+++ src/pages/Blog/components/SearchForm/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.div`
+  width: 864px;
+  margin: auto;
+  margin-top: 72px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+
+    h1 {
+      font-size: 18px;
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+
+    span {
+      font-size: 14px;
+      color: ${(props) => props.theme["base-span"]};
+    }
+  }
+
+  input {
+    width: 864px;
+    height: 50px;
+    margin-top: 12px;
+    background: ${(props) => props.theme["base-input"]};
+    border: 1px solid ${(props) => props.theme["base-border"]};
+    padding: 12px 16px;
+    color: ${(props) => props.theme["base-text"]};
+    border-radius: 6px;
+
+
+    &::placeholder {
+      font-size: 16px;
+      color: ${(props) => props.theme["base-label"]};
+    }
+
+    &:focus{
+    border: 1px solid ${(props) => props.theme["blue"]};
+
+    }
+
+  }
+`;
