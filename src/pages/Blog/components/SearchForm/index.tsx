df6ee40a523df2fb0494d53bd7e1724b6cd1f42c
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-07 19:21:07 -0300

    configurei useForm + zod

--- src/pages/Blog/components/SearchForm/index.tsx
+++ src/pages/Blog/components/SearchForm/index.tsx
@@ -1,13 +1,35 @@
+import { useContext } from "react";
 import { SearchFormContainer } from "./styles";
+import { PostsContext } from "../../../../contexts/PostsContext";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { issues, fetchIssue } = useContext(PostsContext);
+
+  const { register, handleSubmit, reset } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
+
+  async function handleSearchPost(data: SearchFormInputs) {
+    fetchIssue(data.query);
+    reset();
+  }
+
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchPost)}>
       <div>
         <h1>Publicações</h1>
-        <span>6 publicações</span>
+        <span>{issues.length} publicações</span>
       </div>
-      <input type="text" placeholder="Buscar Conteúdo" />
+      <input type="text" placeholder="Buscar Conteúdo" {...register("query")} />
     </SearchFormContainer>
-  )
+  );
 }
